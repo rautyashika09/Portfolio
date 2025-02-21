46702c54ea8211b57454acfaadea4fa431fa0e2a
Yashika_Raut <rautyashikao9@gmail.com>
2025-02-21 11:54:56 +0530

    add files

---
+++ src/costants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/project1.jpg";
+import project2 from "../assets/projects/project2.jpg";
+import project3 from "../assets/projects/project3.jpg";
+
+export const HERO_CONTENT = "Heyy! I'm a Frontend Developer.I specialize in React.js & WordPress, crafting dynamic, user-friendly, and performance-optimized websites. Whether it's building sleek React applications or designing seamless WordPress experiences, I love turning ideas into interactive digital solutions. Passionate about clean UI, smooth UX, and modern web technologies. Let’s build something amazing! "
+
+export const ABOUT ="Hey! I'm a Frontend Developer with expertise in React.js & WordPress. I specialize in building dynamic, responsive, and user-friendly websites that deliver seamless user experiences. From crafting modern React applications to developing customizable WordPress solutions, I focus on performance, scalability, and clean design.Passionate about the latest web technologies, I love turning ideas into functional and engaging digital experiences. Whether it's creating interactive UIs or optimizing website performance, I strive to deliver high-quality solutions that make an impact."
+
+export const EXPERIENCES = [
+    {
+        years: "2024 (December - Present)",
+        role: "React Developer",
+        description:"ave worked as a React & WordPress Developer, gaining experience in live projects by building scalable, high-performance web applications and custom WordPress solutions. My expertise includes React.js, Tailwind CSS and UI/UX optimization, delivering responsive and production-ready solutions for real-world clients.",
+        technologies:["HTML5", "CSS3", "React", "Wordpress",]
+    },
+    {
+        years: "2024 (July - December)",
+        role: "React Developer (WordPress Developer)",
+        description: "I have worked on live React projects, developing and deploying scalable, high-performance, and interactive web applications. With expertise in React.js, JavaScript, Tailwind CSS, API integration, and state management, I have built responsive and user-friendly solutions for real-world clients",
+        technologies:["HTML5", "CSS3", "React"]
+    }
+
+]
+
+export const PROJECTS = [
+    {
+        title: "Obys Clone",
+        description: "I built a pixel-perfect clone of the Obys Agency website using HTML, CSS, JavaScript, GSAP, and Shery.js, focusing on smooth animations, fluid transitions, and an interactive user experience. This project highlights my ability to implement advanced scroll-based animations, seamless motion effects, and responsive design while ensuring performance optimization and precise UI replication. It showcases my expertise in crafting visually engaging and dynamic web experiences with clean, maintainable code.",
+        image: project1,
+       
+    },
+
+    
+        {
+            title: "Portfolio Clone",
+            description: "I built my portfolio website using React and Framer Motion, focusing on smooth animations, seamless interactions, and a modern UI. This project showcases my expertise in React.js, component-based architecture, and motion design to create an engaging and dynamic user experience. With Framer Motion, I implemented fluid transitions, hover effects, and scroll-based animations, ensuring a visually appealing and interactive interface. The website is fully responsive, optimized for performance, and designed to highlight my skills and projects in the best possible way..",
+            image: project2,
+           
+        }, 
+
+        {
+            title: "Printrest page clone",
+            description: "I developed a Pinterest page clone using HTML and CSS, replicating its clean grid layout, responsive design, and seamless UI structure. This project showcases my ability to create pixel-perfect, visually appealing, and user-friendly web pages while maintaining a modern and adaptive design..",
+            image: project3,
+            technologies: ["HTML5", "CSS3"] ,
+        }
+    
+]
